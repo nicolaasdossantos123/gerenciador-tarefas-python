4a4a13128646d3d68677a88cefd6ea4d4ad73aeb
Nicolaas dos Santos <nicolaasdossantos123@gmail.com>
2026-03-30 17:01:59 -0300

    Delete teste directory

--- teste/teste2.py
+++
@@ -1,9 +0,0 @@
-def login(usuario, senha):
-    if usuario == "admin" and senha == "1234":
-        return "Acesso permitido"
-    else:
-        return "Acesso negado"
-    
-print(login("ad13in", "1234"))  
-
-       
